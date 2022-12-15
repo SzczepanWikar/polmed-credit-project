3b991d4488024f1b4a9b33affc7c975691c33035
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2022-12-15 08:20:45 +0100

    post merge fixes

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import './App.scss';
 import { Footer } from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Pages/Home';
-import { GlobalContext, globalData } from './Contexts/GlobalContext';
+import { GlobalContext, doctors, services } from './Contexts/GlobalContext';
 import { DoctorVisit } from './Pages/DoctorVisit/DoctorVisit';
 import { Summary } from './Pages/Summary/Summary';
 export const App = () => {
   return (
     <>
-      <GlobalContext.Provider value={globalData}>
+      <GlobalContext.Provider value={{ doctors: doctors, services: services }}>
         <BrowserRouter>
           <Navbar />
           <main>
--- src/Contexts/GlobalContext.ts
+++ src/Contexts/GlobalContext.ts
@@ -13,62 +13,61 @@ export const GlobalContext = createContext<GlobalData>({
   services: [],
 });
 
-export const globalData: GlobalData = {
-  doctors: [
-    {
-      id: 1,
-      name: 'Jan',
-      lastName: 'Kowalski',
-      specialization: Specialization.familyDoctor,
-      avatar: '',
-    },
-    {
-      id: 2,
-      name: 'Anna',
-      lastName: 'Kowalska',
-      specialization: Specialization.pediatrician,
-      avatar: '',
-    },
-    {
-      id: 3,
-      name: 'Zofia',
-      lastName: 'Nowak',
-      specialization: Specialization.familyDoctor,
-      avatar: '',
-    },
-    {
-      id: 4,
-      name: 'Antoni',
-      lastName: 'Wiśwnieski',
-      specialization: Specialization.pediatrician,
-      avatar: '',
-    },
-  ],
-  services: [
-    {
-      id: 1,
-      name: 'Wizyta kontrolna',
-      price: 100,
-    },
-    {
-      id: 2,
-      name: 'Konsultacja',
-      price: 70,
-    },
-    {
-      id: 3,
-      name: 'Choroba',
-      price: 150,
-    },
-    {
-      id: 4,
-      name: 'Wypisanie recepty',
-      price: 50,
-    },
-    {
-      id: 5,
-      name: 'Szczepienie',
-      price: 50,
-    },
-  ],
-};
+export const doctors: Doctor[] = [
+  {
+    id: 1,
+    name: 'Jan',
+    lastName: 'Kowalski',
+    specialization: Specialization.familyDoctor,
+    avatar: 'jan-kowalski.jpg',
+  },
+  {
+    id: 2,
+    name: 'Anna',
+    lastName: 'Kowalska',
+    specialization: Specialization.pediatrician,
+    avatar: 'anna-kowalska.jpg',
+  },
+  {
+    id: 3,
+    name: 'Zofia',
+    lastName: 'Nowak',
+    specialization: Specialization.familyDoctor,
+    avatar: 'zofia-nowak.jpg',
+  },
+  {
+    id: 4,
+    name: 'Antoni',
+    lastName: 'Wiśwnieski',
+    specialization: Specialization.pediatrician,
+    avatar: 'antoni-wisniewski.jpg',
+  },
+];
+
+export const services: Service[] = [
+  {
+    id: 1,
+    name: 'Wizyta kontrolna',
+    price: 100,
+  },
+  {
+    id: 2,
+    name: 'Konsultacja',
+    price: 70,
+  },
+  {
+    id: 3,
+    name: 'Choroba',
+    price: 150,
+  },
+  {
+    id: 4,
+    name: 'Wypisanie recepty',
+    price: 50,
+  },
+  {
+    id: 5,
+    name: 'Szczepienie',
+    price: 50,
+  },
+];
