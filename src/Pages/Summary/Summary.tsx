bb30169adac81ff20e618a7374029b92d613292b
Krzysztof <krzysztoforek13@gmail.com>
2022-12-20 21:22:45 +0100

    add classnames and buttons

--- src/Pages/Summary/Summary.tsx
+++ src/Pages/Summary/Summary.tsx
@@ -5,13 +5,28 @@ export const Summary = () => {
   return (
 
     <>
-      <div className='header'>
+    <div className='SummaryPage'>
+      <div className='SummaryPage_container'>
         <h1>Potwierdzenie rezerwacji</h1>
+        <div>
+          Doktor: 
+        </div>
+        <div>
+          Cel wizyty:
+        </div>
+        <div>
+          Termin wizyty:
+        </div>
+        <div className='SummaryPage_buttons'>
+          <button className='SummaryPage_buttons_home'>
+            Powrót do strony głównej
+          </button>
+          <button className='SummaryPage_buttons_cancel'>
+            Anuluj
+          </button>
+        </div>
       </div>
-      <div>
-        Doktor: 
-      </div>
-      
+    </div>
     </>
 
   );
