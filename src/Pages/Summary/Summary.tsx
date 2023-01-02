52e589d21d52b524eeae9b4ad850bdbc7526b22e
Krzysztof <krzysztoforek13@gmail.com>
2023-01-03 00:58:15 +0100

    buttons position

--- src/Pages/Summary/Summary.tsx
+++ src/Pages/Summary/Summary.tsx
@@ -8,27 +8,30 @@ export const Summary = () => {
     <div className='SummaryPage'>
       <div className='SummaryPage_container'>
         <h1>Potwierdzenie rezerwacji</h1>
-        <div>
-          Doktor: 
-        </div>
-        <div>
-          Cel wizyty:
-        </div>
-        <div>
-          Termin wizyty:
-        </div>
+        <form>
+          <div>
+            Doktor:
+            Anna Kowalska
+          </div>
+          <div>
+            Cel wizyty:
+            Wizyta kontrolna
+          </div>
+          <div>
+            Termin wizyty:
+            16.01.2023 14:00
+          </div>
+        </form>
         <div className='SummaryPage_buttons'>
           
           <button className='SummaryPage_buttons_back'>
-            Cofnij
+            Powrót
           </button>
-
           <button className='SummaryPage_buttons_home'>
             Powrót do strony głównej
           </button>
-          
           <button className='SummaryPage_buttons_cancel'>
-            Anuluj
+            Anuluj rezerwacje
           </button>
         </div>
       </div>
