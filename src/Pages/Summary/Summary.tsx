b72387e916669f186198228e97765f67b4bd4f45
Krzysztof <krzysztoforek13@gmail.com>
2023-01-02 20:52:22 +0100

    replaced buttons and colors

--- src/Pages/Summary/Summary.tsx
+++ src/Pages/Summary/Summary.tsx
@@ -18,9 +18,15 @@ export const Summary = () => {
           Termin wizyty:
         </div>
         <div className='SummaryPage_buttons'>
+          
+          <button className='SummaryPage_buttons_cancel'>
+            Cofnij
+          </button>
+
           <button className='SummaryPage_buttons_home'>
             Powrót do strony głównej
           </button>
+          
           <button className='SummaryPage_buttons_cancel'>
             Anuluj
           </button>
